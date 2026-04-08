8785edea26286748fd9ee08ac1005470abfbf16e
Hasibur Rahman <113811769+inbox-hasibur@users.noreply.github.com>
2026-04-08 06:10:42 +0600

    fix: explore menu slider and hover styles, updated map location, tweaked mobile footer

--- client/src/components/FindUs/FindUs.jsx
+++ client/src/components/FindUs/FindUs.jsx
@@ -16,7 +16,7 @@ const FindUs = () => {
             </div>
             <div className="find-us-right">
                 <iframe 
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3648.7314643039643!2d90.39572457606622!3d23.863640278593466!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c421711c9bb7%3A0xe212b4eefe92fb38!2sIUBAT%20-%20International%20University%20of%20Business%20Agriculture%20and%20Technology!5e0!3m2!1sen!2sbd!4v1714456789124!5m2!1sen!2sbd" 
+                    src="https://maps.google.com/maps?q=IUBAT%20Lemonlime%20Cafeteria&t=&z=17&ie=UTF8&iwloc=&output=embed" 
                     width="100%" 
                     height="100%" 
                     style={{border:0}} 
